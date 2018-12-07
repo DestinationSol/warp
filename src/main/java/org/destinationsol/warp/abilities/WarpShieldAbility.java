13213b3529d99f0e942e783dd0ad960a53d822a9
Benjamin Amos <benjamin.amos11@gmail.com>
2018-12-07 20:20:25 +0000

    Added the warp shield ability and added a proper icon for the explorer ship.

---
+++ src/main/java/org/destinationsol/warp/abilities/WarpShieldAbility.java
@@ -0,0 +1,116 @@
+package org.destinationsol.warp.abilities;
+
+import com.badlogic.gdx.math.Vector2;
+import org.destinationsol.game.AbilityCommonConfig;
+import org.destinationsol.game.SolGame;
+import org.destinationsol.game.drawables.DrawableLevel;
+import org.destinationsol.game.item.ItemConfig;
+import org.destinationsol.game.item.ItemManager;
+import org.destinationsol.game.item.Shield;
+import org.destinationsol.game.item.SolItem;
+import org.destinationsol.game.particle.DSParticleEmitter;
+import org.destinationsol.game.ship.AbilityConfig;
+import org.destinationsol.game.ship.ShipAbility;
+import org.destinationsol.game.ship.SolShip;
+import org.json.JSONObject;
+
+import java.util.List;
+
+public class WarpShieldAbility implements ShipAbility {
+    private final WarpShieldAbilityConfig config;
+    private Shield originalShield;
+    private float shieldTimer = 0.0f;
+    private boolean warpShieldPresent;
+
+    public WarpShieldAbility(WarpShieldAbilityConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public boolean update(SolGame game, SolShip owner, boolean tryToUse) {
+       if (tryToUse) {
+           originalShield = owner.getShield();
+           if (!warpShieldPresent) {
+               owner.getItemContainer().add(config.warpShield);
+               warpShieldPresent = true;
+           }
+           owner.maybeEquip(game, config.warpShield, true);
+           shieldTimer = config.shieldDuration;
+           return true;
+       }
+
+       if (shieldTimer <= 0) {
+           if (warpShieldPresent) {
+               owner.getItemContainer().remove(config.warpShield);
+               warpShieldPresent = false;
+           }
+           owner.maybeEquip(game, originalShield, true);
+           return false;
+       }
+
+       shieldTimer -= game.getTimeStep();
+       return false;
+    }
+
+    @Override
+    public AbilityConfig getConfig() {
+        return config;
+    }
+
+    @Override
+    public AbilityCommonConfig getCommonConfig() {
+        return config.getCommonConfig();
+    }
+
+    @Override
+    public float getRadius() {
+        return Float.MAX_VALUE;
+    }
+
+    public static class WarpShieldAbilityConfig implements AbilityConfig {
+        private final float rechargeTime;
+        private final float shieldDuration;
+        private final Shield warpShield;
+        private final AbilityCommonConfig commonConfig;
+
+        public WarpShieldAbilityConfig(float rechargeTime, float shieldDuration, Shield warpShield, AbilityCommonConfig commonConfig) {
+            this.rechargeTime = rechargeTime;
+            this.shieldDuration = shieldDuration;
+            this.commonConfig = commonConfig;
+            this.warpShield = warpShield;
+        }
+
+        @Override
+        public ShipAbility build() {
+            return new WarpShieldAbility(this);
+        }
+
+        @Override
+        public SolItem getChargeExample() {
+            return null;
+        }
+
+        @Override
+        public float getRechargeTime() {
+            return rechargeTime;
+        }
+
+        public AbilityCommonConfig getCommonConfig() {
+            return commonConfig;
+        }
+
+        @Override
+        public void appendDesc(StringBuilder sb) {
+            sb.append("Activates the ship's warp shield, which redirects all projectiles in normal space around it.");
+        }
+
+        public static AbilityConfig load(JSONObject abNode, ItemManager itemManager, AbilityCommonConfig cc) {
+            float rechargeTime = (float) abNode.getDouble("rechargeTime");
+            float shieldDuration = abNode.getFloat("shieldDuration");
+            String warpShieldName = abNode.getString("shield");
+            itemManager.parseItems(warpShieldName);
+            Shield shield = (Shield) itemManager.getExample(warpShieldName);
+            return new WarpShieldAbilityConfig(rechargeTime, shieldDuration, shield, cc);
+        }
+    }
+}
